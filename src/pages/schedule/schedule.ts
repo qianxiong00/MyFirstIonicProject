6a50dd0e17b46d57630fbc0796d68ab57ef74bb3
ken <ken@kumaken-no-MacBook.local>
2018-11-28 23:46:07 +0900

    update images

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -16,37 +16,37 @@ export class SchedulePage {
     {
       title: "Turku",
       subTitle: "12/25",
-      img: "assets/imgs/schedule-stockholm.jpg"
+      img: "assets/imgs/schedule-turku.jpg"
     },
     {
       title: "Helsinki",
       subTitle: "12/26",
-      img: "assets/imgs/schedule-stockholm.jpg"
+      img: "assets/imgs/schedule-helsinki.jpg"
     },
     {
       title: "Tromso",
       subTitle: "12/27 ~ 12/30",
-      img: "assets/imgs/schedule-stockholm.jpg"
+      img: "assets/imgs/schedule-tromso.jpg"
     },
     {
       title: "Bergen",
       subTitle: "12/31",
-      img: "assets/imgs/schedule-stockholm.jpg"
+      img: "assets/imgs/schedule-bergen.jpg"
     },
     {
       title: "Flam",
       subTitle: "01/01",
-      img: "assets/imgs/schedule-stockholm.jpg"
+      img: "assets/imgs/schedule-flam.jpg"
     },
     {
       title: "Oslo",
       subTitle: "01/02",
-      img: "assets/imgs/schedule-stockholm.jpg"
+      img: "assets/imgs/schedule-oslo.jpg"
     },
     {
       title: "Copenhagen",
       subTitle: "01/02 ~ 01/06",
-      img: "assets/imgs/schedule-stockholm.jpg"
+      img: "assets/imgs/schedule-copenhagen.jpg"
     }
   ]
 
