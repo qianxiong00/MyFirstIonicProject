13e7b4d1682d3993196064c0eb14d8d3ee94b0ee
acn-qian-xiong <35250948+acn-qian-xiong@users.noreply.github.com>
2018-11-28 17:53:48 +0900

    update schedule page

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -5,6 +5,49 @@ import { Component } from '@angular/core';
   templateUrl: 'schedule.html'
 })
 export class SchedulePage {
+  items: Array<{title:string, subTitle:string, img:string}> = [
+    {
+      title: "Stockholm",
+      subTitle: "12/22 ~ 12/24",
+      img: "assets/imgs/schedule-stockholm.jpg"
+    },
+    {
+      title: "Turku",
+      subTitle: "12/25",
+      img: "assets/imgs/schedule-stockholm.jpg"
+    },
+    {
+      title: "Helsinki",
+      subTitle: "12/26",
+      img: "assets/imgs/schedule-stockholm.jpg"
+    },
+    {
+      title: "Tromso",
+      subTitle: "12/27 ~ 12/30",
+      img: "assets/imgs/schedule-stockholm.jpg"
+    },
+    {
+      title: "Bergen",
+      subTitle: "12/31",
+      img: "assets/imgs/schedule-stockholm.jpg"
+    },
+    {
+      title: "Flam",
+      subTitle: "01/01",
+      img: "assets/imgs/schedule-stockholm.jpg"
+    },
+    {
+      title: "Oslo",
+      subTitle: "01/02",
+      img: "assets/imgs/schedule-stockholm.jpg"
+    },
+    {
+      title: "Copenhagen",
+      subTitle: "01/02 ~ 01/06",
+      img: "assets/imgs/schedule-stockholm.jpg"
+    }
+  ]
+
   constructor() {
   }
 }
