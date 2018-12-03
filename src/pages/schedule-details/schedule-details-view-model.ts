b6e411d9eb795cb100fc39d08363c29da4a9115f
System Administrator <root@kumaken-no-MacBook.local>
2018-12-03 11:09:10 +0900

    update source

--- src/pages/schedule-details/schedule-details-view-model.ts
+++ src/pages/schedule-details/schedule-details-view-model.ts
@@ -93,12 +93,131 @@ export function ScheduleDetailsInitialState():ScheduleDetailsViewModel {
         title: '12/23',
         detail_list: [
             {
-            content: 'schedule2 content',
-            date_time: 'CET 14:00',
+            content: 'Biz Apartmentホテル',
+            date_time: '中央ヨーロッパ時間CET 09:00',
             icon_name: 'pin',
-            position: {lat: 20, lng: 20},
-            trafic: 'plane',
-            time: '11時間35分'
+            position: {lat: 59.301942, lng: 18.098854},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ヒョートリエット（Hötorget）広場',
+            date_time: '日曜日はロッピス「蚤の市」が開催',
+            icon_name: 'pin',
+            position: {lat: 59.334719, lng: 18.063052},
+            trafic: 'train',
+            time: ''
+        },{
+            content: '歩行者天国セルゲルガータン通り',
+            date_time: 'Sergelgatan',
+            icon_name: 'pin',
+            position: {lat: 59.334014, lng: 18.063323},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ガムラスタン',
+            date_time: 'Gamla stan',
+            icon_name: 'pin',
+            position: {lat: 59.325658, lng: 18.070836},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ノーベル博物館',
+            date_time: 'Nobelmuseet 120 SEK',
+            icon_name: 'pin',
+            position: {lat: 59.325369, lng: 18.070857},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'モーテン・トローツィグ通り',
+            date_time: 'Mårten Trotzigs gränd',
+            icon_name: 'pin',
+            position: {lat: 59.323003, lng: 18.072766},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ストックホルム王宮 Kungliga slottet 160 SEK',
+            date_time: '中央ヨーロッパ時間CET 13:00',
+            icon_name: 'pin',
+            position: {lat: 59.326898, lng: 18.071752},
+            trafic: 'train',
+            time: ''
+        },{
+            content: '大聖堂',
+            date_time: 'Storkyrkan 60 SEK',
+            icon_name: 'pin',
+            position: {lat: 59.325782, lng: 18.070422},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ドイツ教会',
+            date_time: 'Tyska Kyrkan',
+            icon_name: 'pin',
+            position: {lat: 59.324147, lng: 18.071781},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'カステルホルメン島',
+            date_time: 'Kastellholmen',
+            icon_name: 'pin',
+            position: {lat: 59.322605, lng: 18.089398},
+            trafic: 'train',
+            time: ''
+        },{
+            content: '現代美術館',
+            date_time: 'Moderna Museet Free/100/150',
+            icon_name: 'pin',
+            position: {lat: 59.326118, lng: 18.084709},
+            trafic: 'train',
+            time: ''
+        },{
+            content: '王冠の橋',
+            date_time: 'Gilded Crown on Skeppsholmsbron',
+            icon_name: 'pin',
+            position: {lat: 59.327341, lng: 18.079030},
+            trafic: 'train',
+            time: ''
+        },{
+            content: '国立美術館',
+            date_time: 'Nationalmuseum 150 SEK',
+            icon_name: 'pin',
+            position: {lat: 59.328501, lng: 18.078329},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ストックホルム市庁舎',
+            date_time: 'The City Hall',
+            icon_name: 'pin',
+            position: {lat: 59.327483, lng: 18.054346},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'スカンセン',
+            date_time: 'Skansen 125 SEK',
+            icon_name: 'pin',
+            position: {lat: 59.327013, lng: 18.103732},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'スカンセン・ブティーケン',
+            date_time: 'SKANSENBUTIKEN',
+            icon_name: 'pin',
+            position: {lat: 59.324615, lng: 18.100463},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ストックホルム・グローブ・アリーナ',
+            date_time: 'Skyview 150 SEK',
+            icon_name: 'pin',
+            position: {lat: 59.293580, lng: 18.083550},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'Biz Apartmentホテル',
+            date_time: '中央ヨーロッパ時間CET 20:00',
+            icon_name: 'pin',
+            position: {lat: 59.301942, lng: 18.098854},
+            trafic: '',
+            time: ''
         }]
     },
     {
@@ -107,12 +226,75 @@ export function ScheduleDetailsInitialState():ScheduleDetailsViewModel {
         title: '12/24',
         detail_list: [
             {
-            content: 'schedule3 content',
-            date_time: 'CET 15:00',
+            content: 'Biz Apartmentホテル',
+            date_time: '中央ヨーロッパ時間CET 09:00',
+            icon_name: 'pin',
+            position: {lat: 59.301942, lng: 18.098854},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'グラニート',
+            date_time: 'Granit',
             icon_name: 'pin',
             position: {lat: 20, lng: 20},
-            trafic: 'plane',
-            time: '11時間35分'
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'デザイン・トリエ',
+            date_time: 'DesignTorget',
+            icon_name: 'pin',
+            position: {lat: 20, lng: 20},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'オーニング＆レダ',
+            date_time: 'Ordning & Reda',
+            icon_name: 'pin',
+            position: {lat: 20, lng: 20},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ロイヤルカナルツアー（Royal Canal Tour）',
+            date_time: '中央ヨーロッパ時間CET 10:30-11:30',
+            icon_name: 'pin',
+            position: {lat: 20, lng: 20},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ストックホルム市立図書館',
+            date_time: 'Stockholms Stadsbibliotek',
+            icon_name: 'pin',
+            position: {lat: 20, lng: 20},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'セーデルマルム島',
+            date_time: 'Södermalm',
+            icon_name: 'pin',
+            position: {lat: 20, lng: 20},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'ドロットニングホルム宮殿',
+            date_time: '',
+            icon_name: 'pin',
+            position: {lat: 20, lng: 20},
+            trafic: 'train',
+            time: ''
+        },{
+            content: 'フェリーターミナル Stockholm Vartahamnen',
+            date_time: '中央ヨーロッパ時間CET 19:30',
+            icon_name: 'pin',
+            position: {lat: 20, lng: 20},
+            trafic: 'boat',
+            time: '11時間'
+        },{
+            content: 'トゥルクフェリーターミナルTurku harbor',
+            date_time: '東ヨーロッパ時間EET 07:00',
+            icon_name: 'pin',
+            position: {lat: 20, lng: 20},
+            trafic: '',
+            time: ''
         }]
     },
     {
--- src/pages/schedule/schedule-view-model.ts
+++ src/pages/schedule/schedule-view-model.ts
@@ -1,5 +1,12 @@
 export class ScheduleViewModel {
-     items: Array<{id:string, title:string, subTitle:string, img:string, color:string}>
+     items: Array<{
+       id:string,
+       title:string,
+       subTitle:string,
+       img:string,
+       color:string,
+       background:string
+      }>
 }
 
 export function ScheduleInitialState():ScheduleViewModel {
@@ -10,56 +17,64 @@ export function ScheduleInitialState():ScheduleViewModel {
               title: "Stockholm",
               subTitle: "12/22 ~ 12/24",
               img: "assets/imgs/schedule-stockholm1.png",
-              color:"#ffffff"
+              color:"#03e2af",
+              background:"rgba(3, 226, 175, 0.04)"
             },
             {
               id: "02",
               title: "Turku",
               subTitle: "12/25",
               img: "assets/imgs/schedule-turku1.png",
-              color:"#ff8080"
+              color:"#ff8080",
+              background:"rgba(255, 128, 128, 0.04)"
             },
             {
               id: "03",
               title: "Helsinki",
               subTitle: "12/26",
               img: "assets/imgs/schedule-helsinki1.png",
-              color:"#e2ffa0"
+              color:"#e2ffa0",
+              background:"rgba(226, 255, 160, 0.04)"
             },
             {
               id: "04",
               title: "Tromso",
               subTitle: "12/27 ~ 12/30",
               img: "assets/imgs/schedule-tromso1.png",
-              color:"#f9b7f3"
+              color:"#f9b7f3",
+              background:"rgba(249, 183, 243, 0.04)"
             },
             {
               id: "05",
               title: "Bergen",
               subTitle: "12/31",
               img: "assets/imgs/schedule-bergen1.png",
-              color:"#1c15f1"
+              color:"#1c15f1",
+              background:"rgba(28, 21, 241, 0.04)"
             },
             {
               id: "06",
               title: "Flam",
               subTitle: "01/01",
               img: "assets/imgs/schedule-flam1.png",
-              color:"#ffffff"
+              color:"#bfd604",
+              background:"rgba(191, 214, 4, 0.04)"
             },
             {
               id: "07",
               title: "Oslo",
               subTitle: "01/02",
               img: "assets/imgs/schedule-oslo1.png",
-              color:"#bd00b4"
+              color:"#bd00b4",
+              background:"rgba(189, 0, 180, 0.04)"
             },
             {
               id: "08",
               title: "Copenhagen",
               subTitle: "01/02 ~ 01/06",
               img: "assets/imgs/schedule-copenhagen1.png",
-              color:"#e9fbdf"
+              color:"#f9ba09",
+              background:"rgba(249, 186, 9, 0.04)"
             }
         ]
       }
